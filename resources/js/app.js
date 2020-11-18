e44d715c99fedf5f28abec7a493cde5a6d5e3c17
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-11-18 21:27:05 +0300

    Jetstream with Livewire and new Vue 3 with Laravel mix to work in updated environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import { createApp } from 'vue';
+import categoryForm from './components/categoryForm.vue';
+
+
+createApp({
+  components: {
+    categoryForm,
+  }
+}).mount('#app');
