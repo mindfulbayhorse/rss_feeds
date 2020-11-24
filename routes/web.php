5c4071976348a1a952866a7fc6da1a247093877e
Olga Zhilkova <mindfulbayhorse@gmail.com>
2020-11-24 19:00:22 +0300

    base configuration for unit test

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('rss', RssFeedController::class);
+Route::resource('rss', RssFeedController::class)->middleware('auth');
 Route::resource('categories', CategoryController::class)->middleware('auth');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
