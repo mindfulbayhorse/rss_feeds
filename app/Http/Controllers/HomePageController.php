38d0445652e3e1999387fdae5d594879fefcd087
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-04-07 19:14:01 +0300

    Request for home page to know if user is authenticated

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomePageController extends Controller
 {
     
-    public function show(){
+    public function show(Request $request){
 
         return view('welcome', [ 'jwplayer' => app('jwplayer')]);
     }
