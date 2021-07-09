377ba57d813ec08f689f0e6cc314a2df3a3ef599
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-07-09 19:03:16 +0300

    new events with custom rss creating event

---
+++ app/Events/ParseRss.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ParseRss
+{
+    use Dispatchable, SerializesModels;
+    
+    public $rss;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($rss)
+    {
+        $this->rss = $rss;
+    }
+
+}
