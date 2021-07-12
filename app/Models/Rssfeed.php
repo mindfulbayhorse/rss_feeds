aaf8c4a47419e602560ec4e947f3797d03904a84
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-07-12 22:04:08 +0300

    action for rss to check if rss is updated

--- app/Models/Rssfeed.php
+++ app/Models/Rssfeed.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Events\ParseRss;
+use Carbon\Carbon;
 
 class Rssfeed extends Model
 {
@@ -26,26 +27,58 @@ class Rssfeed extends Model
     	return $date == strtotime($this->updated_at);
     }
     
-    public function findByURL($query, $url){
+    public function findByURL($query, $url)
+    {
     	
     	return $query->where('url', $url);
     	
     }
     
-    public function user(){
+    public function user()
+    {
     	
     	return $this->belongsTo(User::class);
     }
     
-    public function category(){
+    public function category()
+    {
     	
     	return $this->belongsTo(Category::class);
     }
     
-    public function add($user = null){
+    public function add($user = null)
+    {
         
         $this->user_id = $user ? $user->id : auth()->id;
         
+        $this->save();
+    }
+    
+    public function checkLastUpdate()
+    {
+        $reader = new \XMLReader();
+        $result = $reader->open($this->url);
+        
+        if (!$result) {
+            $reader->close();
+            return;
+        }
+        
+        while ($reader->read()) {
+            if ($reader->name === 'lastBuildDate') {
+                $rssUpdate = $reader->readString();
+                break;
+            }
+        }
+        
+        if (!$rssUpdate) {
+            $reader->close();
+            return;
+        }
+        
+        $reader->close();
+        
+        $this->last_update=Carbon::createFromFormat(\DateTime::RSS, $rssUpdate);
         $this->save();
     }
     
