6bc56d3b3202de0a29784839ca805048b5d6e243
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-07-16 11:09:59 +0300

    creating job to parse rss url for checking last update

---
+++ app/Jobs/ProcessRss.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Rssfeed;
+
+class ProcessRss implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $rss;
+    
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Rssfeed $rss)
+    {
+        $this->rss = $rss;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        if ($this->rss->checkLastUpdate()){
+            //send notification
+        }
+    }
+}
