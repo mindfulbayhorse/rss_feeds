117452fa95de835a18b495b657979c3ebc2aa520
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-07-19 20:05:20 +0300

    updating last update field for rss when parsing

--- app/Jobs/ProcessRss.php
+++ app/Jobs/ProcessRss.php
@@ -3,18 +3,20 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Rssfeed;
+use Carbon\Carbon;
 
 class ProcessRss implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $rss;
+    
+    public $tries = 3;
     
     /**
      * Create a new job instance.
@@ -24,6 +26,7 @@ class ProcessRss implements ShouldQueue
     public function __construct(Rssfeed $rss)
     {
         $this->rss = $rss;
+        
     }
 
     /**
@@ -33,9 +36,40 @@ class ProcessRss implements ShouldQueue
      */
     public function handle()
     {
-
-        if ($this->rss->checkLastUpdate()){
-            //send notification
+        $reader = new \XMLReader();
+       
+  
+        $result = $reader->open($this->rss->url);
+        
+        if (!$result) {
+                return;
         }
+        
+        while ($reader->read()) {
+            if ($reader->name === 'lastBuildDate') {
+                $rssUpdate = $reader->readString();
+                break;
+            }
+        }
+        
+        if (!$rssUpdate) {
+            $reader->close();
+
+        }
+        
+        $reader->close();
+ 
+        $updatingDate = Carbon::createFromFormat(\DateTime::RSS, $rssUpdate);
+        
+        if (!is_null($this->rss->last_update)){
+            if ($this->rss->last_update->equalTo($updatingDate)){
+                return;
+            }
+        }
+        
+        $this->rss->last_update=$updatingDate;
+        
+        $this->rss->save();
+
     }
 }
