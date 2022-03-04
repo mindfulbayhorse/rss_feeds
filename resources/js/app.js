dbf9d510736802c69c685a332089e54ad5d60271
Olga Zhilkova <mindfulbayhorse@gmail.com>
2022-03-04 23:41:40 +0300

    Updated Jetstream scripts and styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,3 @@
 require('./bootstrap');
+
 require('alpinejs');
-
-window.Vue = require('vue');
-
-import { createApp } from 'vue';
-import categoryForm from './components/categoryForm.vue';
-import dropdown from './components/Dropdown.vue';
-
-
-createApp({
-  components: {
-    categoryForm,
-    dropdown
-  }
-}).mount('#rss_app');
